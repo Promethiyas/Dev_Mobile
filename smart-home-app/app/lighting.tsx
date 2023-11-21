9193c27fe473298b0ff0042e5579d7b3a5156630
Promethiyas <valentin.bravo@ynov.com>
2023-11-21 13:48:52 +0100

    add img

--- smart-home-app/app/lighting.tsx
+++ smart-home-app/app/lighting.tsx
@@ -1,29 +1,16 @@
-import { StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View,Image} from 'react-native';
 import { Link } from "expo-router";
 
 export default function Lightning(){
   return (
-    <View style= {{flex: 1,backgroundColor: '#FFFAFA'}}>
-      <Text style={{marginLeft: "40%", fontWeight: "bold", fontSize: 22, color: "brown"}}>{"Lighting"}</Text>
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Link href={"/"}><Text>{"Retour"}</Text></Link>
+    <View style= {{flex: 1,backgroundColor: '#0D2639'}}>
+      <View style= {{flexDirection:"row"}}>
+        <Link href="/"><Text style={{color:"white", marginLeft:"5%", fontSize:25}}>{"< Lighting"}</Text></Link>
+        <Text style={{marginLeft: "40%", fontWeight: "bold", fontSize: 22, color: "brown"}}>{"Lighting"}</Text>
       </View>
-      <View style={{flex: 1}}>
-          <View style={{position: 'absolute', left: 0, right: 0, bottom: 0, padding: "5%", borderRadius: 11, flexDirection: "row",backgroundColor: "white"}}>
-            <View style={styles.footerview}>
-              <Link href={"/"}><Text style={styles.footerlink}>🏠</Text></Link>
-            </View>
-            <View style={styles.footerview}>
-              <Link href={"/scenario"}><Text style={styles.footerlink}>☀️</Text></Link>
-            </View>
-            <View style={styles.footerview}>
-              <Link href={"/notifications"}><Text style={styles.footerlink}>🔔</Text></Link>
-            </View>
-            <View style={styles.footerview}>
-              <Link href={"/user"}><Text style={styles.footerlink}>🪪</Text></Link>
-            </View>
-          </View>
-        </View>
+      <View>
+        <Image source={require('../ressources/ampoule.png')}></Image>
+      </View>
     </View>
   )
 }
