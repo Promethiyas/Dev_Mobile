e1a497cdadf8076049d0954c4711cb01b433f0cb
Promethiyas <valentin.bravo@ynov.com>
2023-11-28 11:26:31 +0100

    j'ai finis la deuxieme truc important

--- smart-home-app/app/lighting.tsx
+++ smart-home-app/app/lighting.tsx
@@ -2,26 +2,53 @@ import { StyleSheet, Text, View,Image,TouchableOpacity,Alert, Switch} from 'reac
 import React, {  useState, useEffect } from 'react';
 import { Link } from "expo-router";
 import { LinearGradient } from 'expo-linear-gradient';
+
+const whiteAsset = require('../ressources/ampouleallume.png');
+const whiteInActiveAsset = require('../ressources/ampoule.png')
+const purpleAsset = require('../ressources/ampouleallumeViolet.png');
+const blueAsset = require('../ressources/ampouleallumebleu.png')
+const greenAsset = require('../ressources/ampouleallumeVert.png')
+const redAsset = require('../ressources/ampouleallumeRouge.png')
+
+const ampoule = { 
+  white: {
+  inactiveAsset: whiteInActiveAsset,
+  activeAsset: whiteAsset,
+}, purple: {
+  inactiveAsset: whiteInActiveAsset,
+  activeAsset: purpleAsset,
+}, blue: {
+  inactiveAsset: whiteInActiveAsset,
+  activeAsset: blueAsset,
+}, green: {
+  inactiveAsset: whiteInActiveAsset,
+  activeAsset: greenAsset,
+}, red: {
+  inactiveAsset: whiteInActiveAsset,
+  activeAsset: redAsset,
+} }
+
+
 export default function Lightning(){
   const [isEnabled, setIsEnabled] = useState(false);
    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-  const white = require('../ressources/ampoule.png');
-  const purple = require('../ressources/ampoulepurple.png');
-  const blue = require('../ressources/ampoulebleu.png')
-  const green = require('../ressources/ampouleverte.png')
-  const red = require('../ressources/ampoulerouge.png')
-  const ampoule = { white, purple, blue, green, red }
+  
 
   const [selected, setSelected] = useState(ampoule.white)
   return (
-    <View style= {{flex: 1,backgroundColor: '#0D2639'}}>
+    //#0D2639
+    <View style= {{flex: 1,backgroundColor: '#0D2639'}}> 
       <View style= {{flexDirection:"row"}}>
         <Link href="/"><Text style={{color:"white", fontSize:25}}>{"< Lighting"}</Text></Link>
         <Text style={{marginLeft: "40%", fontWeight: "bold", fontSize: 22, color: "brown"}}>{"Lighting"}</Text>
       </View> 
       <View style={{flex:1,justifyContent: 'center',alignItems: 'center'}}>
-      {isEnabled === true ? <View style={{ position: 'absolute', elevation: 20, shadowColor: 'red', height: 180, width: 150 }}></View>: null}
-        <Image source={selected} style={{height:200, width:150}}/>
+        {isEnabled === true ? 
+          <Image source={selected.activeAsset} style={{height:500, width:375}}/> 
+        : null}
+        {isEnabled === false ? 
+          <Image source={selected.inactiveAsset} style={{height:400, width:300, marginTop: '20%'}}/> 
+        : null}
       </View>
        <LinearGradient start={[0, 0]} end={[1, 1]} colors={['#1C4C9E', '#00F0FF']} style={styles.gradient}>
         <View style={styles.square}>
@@ -32,7 +59,10 @@ export default function Lightning(){
       <View style={{position: 'absolute', left: 0, right: 0, bottom: 0, padding: "5%", borderRadius: 11,backgroundColor: "white",height:"15%"}}>
         <Text>Light Color</Text>
         <View style={{flexDirection: "row"}}>
-          <View style={{marginTop:"5%",marginLeft:"10%"}}><TouchableOpacity onPress={() => setSelected(ampoule.red)} style={{backgroundColor:"red", borderRadius:100, height:30,width:30,borderWidth:1}}></TouchableOpacity></View>
+          <View style={{marginTop:"5%",marginLeft:"10%"}}>
+            <TouchableOpacity onPress={() => setSelected(ampoule.red)} style={{backgroundColor:"red", borderRadius:100, height:30,width:30,borderWidth:1}}>
+            </TouchableOpacity>
+          </View>
           <View style={{marginTop:"5%",marginLeft:"10%"}}><TouchableOpacity onPress={() => setSelected(ampoule.green)} style={{backgroundColor:"green", borderRadius:100, height:30,width:30,borderWidth:1}}></TouchableOpacity></View>
           <View style={{marginTop:"5%",marginLeft:"10%"}}><TouchableOpacity onPress={() => setSelected(ampoule.white)} style={{backgroundColor:"white", borderRadius:100, height:30,width:30,borderWidth:1}}></TouchableOpacity></View>
           <View style={{marginTop:"5%",marginLeft:"10%"}}><TouchableOpacity onPress={() => setSelected(ampoule.blue)} style={{backgroundColor:"blue", borderRadius:100, height:30,width:30,borderWidth:1}}></TouchableOpacity></View>
@@ -59,19 +89,6 @@ const styles = StyleSheet.create({
       marginBottom:"35%",
       marginRight:"50%",
       width:150,
-      height:75
-  },
-  elevationLow: {
-    shadowColor: "red",
-    shadowOffset: {
-      width: 0,
-      height: 7,
-    },
-    shadowOpacity: 0.41,
-    shadowRadius: 9.11,
-    elevation: 14,
-    marginTop:"5%",
-    marginLeft:"10%",
-    backgroundColor: 'red'
-  },
+      height:75,
+  },  
 });
